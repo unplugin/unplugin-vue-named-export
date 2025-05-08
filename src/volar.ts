fa25c30ed967cb8f9c3870ef92eed19c91cfe941
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 02:24:42 +0800

    fix: cjs default interop for volar

--- src/volar.ts
+++ src/volar.ts
@@ -27,3 +27,4 @@ const plugin: VueLanguagePlugin = ({ vueCompilerOptions }) => {
 
 // eslint-disable-next-line import/no-default-export
 export default plugin
+export { plugin as 'module.exports' }
